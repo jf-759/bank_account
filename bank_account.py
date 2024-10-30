9cd35f42fb8b127e399103849f66415fe8b5b6a1
jf-759 <jane.fernandez@students.dominican.edu>
2024-10-29 22:49:02 -0700

    completed project

--- bank_account.py
+++ bank_account.py
@@ -1,14 +1,5 @@
 import random
 
-# class BankAccount:
-#     pass
-
-# account_1 = BankAccount()
-# account_2 = BankAccount()
-# account_3 = BankAccount()
-# account_4 = BankAccount()
-# account_5 = BankAccount()
-
 class BankAccount:
     def __init__(self, name):
         self.full_name = name
