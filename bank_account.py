4b7cb57d7dd4c7e8f627a255f54adb84e52988e1
jf-759 <jane.fernandez@students.dominican.edu>
2024-10-30 19:01:04 -0700

    fixed certain code

--- bank_account.py
+++ bank_account.py
@@ -1,18 +1,25 @@
 import random
 
 class BankAccount:
-    def __init__(self, name):
+    def __init__(self, name, account_type):
         self.full_name = name
         self.account_number = random.randint(10000000, 99999999)
         self.balance = 0
+        self.account_type = account_type
+        self.set_account_type(account_type)
+
+    def display_account_info(self):
+        print(f"Account Holder {self.full_name}")
+        print(f"Account Number: {self.account_number}")
+        print(f"Account Type: {self.account_type.capitalize() if self.account_type else 'Not Set'}")
+        print(f"Balance: ${self.balance:.2f}")
 
     def deposit(self, amount):
         self.balance += amount
         print(f"Amount deposited: ${amount} | New balance: ${self.balance}")
 
     def withdraw(self, amount):
-        self.balance -= amount
-        if self.balance < 0:
+        if amount > self.balance:
             print("Insufficient funds. You will now be charged with an overdraft fee of $10.")
             self.balance -= 10
         else:
@@ -22,7 +29,8 @@ class BankAccount:
         print(f"Your current balance is: ${self.balance}.")
         return self.balance
 
-    def add_interest(self):
+    def add_interest(self, account_type):
+    
         interest = self.balance * 0.00083
         self.balance += interest
         self.balance = round(self.balance, 2)
@@ -32,34 +40,3 @@ class BankAccount:
     def print_statement(self):
         print(f"{self.full_name}\nAccount No.:{self.account_number}\nBalance: ${self.balance}")
 
-#3 Different Bank Accounts:
-
-#1 Mitchell Hudson
-print("\n")
-user_mitchell = BankAccount("Mitchell Hudson")
-user_mitchell.deposit(400000)
-user_mitchell.withdraw(150)
-user_mitchell.get_balance()
-user_mitchell.add_interest()
-user_mitchell.print_statement()
-print("\n")
-
-#2 Christopher Bang
-
-user_chris = BankAccount("Chris Bang")
-user_chris.deposit(8002300)
-user_chris.withdraw(230)
-user_chris.get_balance()
-user_chris.add_interest()
-user_chris.print_statement()
-print("\n")
-
-#3 Jane Fernandez
-
-user_jane = BankAccount("Jane Fernandez")
-user_jane.deposit(7109207)
-user_jane.withdraw(207)
-user_jane.get_balance()
-user_jane.add_interest()
-user_jane.print_statement()
-print("\n")
